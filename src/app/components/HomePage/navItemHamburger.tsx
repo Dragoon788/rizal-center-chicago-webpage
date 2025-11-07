73f6b257645b132871421d60aa31c79f18cef187
Dragoon788 <109482739+Dragoon788@users.noreply.github.com>
2025-11-07 17:07:30 -0600

    add dropdown to hamburger menu

--- src/app/components/HomePage/navItemHamburger.tsx
+++ src/app/components/HomePage/navItemHamburger.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import { NavSection } from './navMenu'
 import { useState } from 'react'
 
+import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/24/solid'
+
 interface NavItemHamburgerProps {
     navCat: NavSection;
 }
@@ -17,10 +19,14 @@ export default function NavItemHamburger ({ navCat } : NavItemHamburgerProps) {
     } 
 
     return(
-        <div className="text-[#F70B54] m-3 mr-10" onMouseDown={handleToggle}>
-            <Link href={`/${navCat.primaryLink.toLowerCase()}`} className="hover:opacity-50">
-                {navCat.primaryLink}
-            </Link>
+        <div className="text-[#F70B54] w-40 m-3 " onMouseDown={handleToggle}>
+            <button className="flex flex-row justify-between w-full">
+                <Link href={`/${navCat.primaryLink.toLowerCase()}`} className="hover:opacity-50">
+                    {navCat.primaryLink}
+                </Link>
+                <ChevronDownIcon className={`h-5 w-5 mr-2 ${(navCat.secondaryLinks.length==0||open)&&"hidden"}`}/>
+                <ChevronUpIcon className={`h-5 w-5 mr-2 ${!open&&"hidden"}`}/>
+            </button>
             <div className={`pt-5 ${!open&&"hidden"}`}>
                 <ul className={"flex flex-col"}>
                     {/* Maybe instead of links to separate pages, I could have
