aea02b2ba2f07fce3bf422409326889201a2a505
Dragoon788 <109482739+Dragoon788@users.noreply.github.com>
2025-11-19 11:43:55 -0600

    pushing changes for now but I'm not sure if I'll continue using swiper

--- src/app/components/HomePage/eventsViewer.tsx
+++ src/app/components/HomePage/eventsViewer.tsx
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from "next/image";
 // import Event from "";
 
-import { Grid, Pagination, Navigation } from 'swiper/modules'
+import { Grid, Pagination, Navigation, Scrollbar } from 'swiper/modules'
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
@@ -13,21 +13,24 @@ interface EventsViewerProps {
 
 export default function EventsViewer ( {upcomingEvents} : EventsViewerProps){
     return(
-        <div className="ml-auto mr-auto w-250 m-5 bg-green-500">
+        <div className="ml-auto mr-auto w-3/4 m-5 bg-green-500">
             <Swiper
                 // pagination={true}
-                navigation={true}
+                // navigation={true}
                 // centeredSlides={true}
                 slidesPerView={3}
                 grid={{
                     rows:1
                 }}
                 centeredSlides={true}
-                modules= {[Grid, Pagination, Navigation]}
+                spaceBetween={20}
+                scrollbar={true}
+                // mousewheel={true}
+                modules= {[Grid, Pagination, Navigation, Scrollbar]}
             >
                 {upcomingEvents.map((eventInfo, i) => 
                     <SwiperSlide key={i} className="">
-                        <div className="flex items-center justify-center w-70 h-40 bg-blue-500">
+                        <div className="flex items-center justify-center h-20 md:h-40 bg-blue-500">
                             {eventInfo}
                         </div>
                     </SwiperSlide>
